fd8e4eb60eef72fd6aec2834830d094b8875cc8d
VenBak <andre.brahin@gmail.com>
2024-09-19 14:15:57 +0200

    fix files import

--- build/CMakeFiles/assignment1.dir/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/assignment1.dir/main.cpp.o: \
-  /Users/andrebrahin/Computing_Science/assignment1/main.cpp
--- main.cpp
+++
@@ -1,6 +0,0 @@
-extern int shell(bool prompt);
-
-int main(int argc, char** argv) {
-	bool showPrompt = argc == 1;
-	return shell(showPrompt);
-}
